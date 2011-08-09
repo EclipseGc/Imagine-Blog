e6ea5f7847362aa91731ff5ab3f5b055059a361d
Kris <kris@meridian-ds.com>
2011-08-09 10:02:58 -0500

    updating the blog with new content_type and layout plugins as well as updating the blog path and taxonomy terms with new layouts

---
+++ plugins/panels/layouts/blog_list/imagine-blog-blog-list.tpl.php
@@ -0,0 +1,7 @@
+<div class="imagine-blog blog-list clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="imagine-blog-panel">
+    <div id="imagine-blog-blog-list-main"><?php print $content['main']; ?></div>
+    <div id="imagine-blog-blog-list-secondary"><?php print $content['secondary']; ?></div>
+    <div id="imagine-blog-blog-list-list"><?php print $content['list']; ?></div>
+  </div>
+</div>
